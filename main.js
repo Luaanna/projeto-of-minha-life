3d795ade6caff8233d87136e5873551fd366166e
Luaanna <142031681+Luaanna@users.noreply.github.com>
2025-06-06 08:29:21 -0300

    main.js

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0; i < botoes.length; i++){
-    botoes[i].onclick=function(){
-        for (let j=0; j < botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
-        botoes[i].classList.add("ativo")
+        textos[i].classList.add("ativo");
     }
 }
 
-
-
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-11-16T00:00:00");
-const tempoObjetivo2 = new Date("2025-05-31T00:00:00");
-const tempoObjetivo3 = new Date("2025-08-16T00:00:00");
-const tempoObjetivo4 = new Date("2026-03-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-11-16T00:00:00"); // 6 meses
+const tempoObjetivo2 = new Date("2025-05-31T00:00:00"); // 15 dias
+const tempoObjetivo3 = new Date("2025-08-16T00:00:00"); // 2 meses
+const tempoObjetivo4 = new Date("2025-06-29T00:00:00"); // 11 meses
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-function calculaTempo(tempoObjetivo){
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
-}
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24)
+
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
@@ -43,15 +42,16 @@ function calculaTempo(tempoObjetivo){
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementByID("dias" + i).textContent = calcularTempo(tempo[tempo[i])[0];
-        document.getElementByID("horas" + i).textContent = calcularTempo(tempo[tempo[i])[1];
-        document.getElementByID("minutos" + i).textContent = calcularTempo(tempo[tempo[i])[2];
-        document.getElementByID("segundos" + i).textContent = calcularTempo(tempo[tempo[i])[3];
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
 function comecaCronometro() {
     atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
+   
 }
 comecaCronometro();
